434786bf31574ce3af5ca55cc827b46982a6a842
Rahul Badenkal <rahulbadenkal@gmail.com>
2024-09-21 17:43:53 +0530

    removed hardocidng env for frontend in vite

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,6 +7,16 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// set env variables -> https://github.com/vitejs/vite/issues/562#issuecomment-754971271
+const viteEnv: any = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
+// For excalidraw
+viteEnv['process.env'] = {}
+
 export default defineConfig({
   plugins: [solid(), solidSvg({defaultAsComponent: true,})],
   server: {
@@ -17,9 +27,7 @@ export default defineConfig({
       "@": resolve(__dirname, "./src")
     }
   },
-  define: {
-    'process.env': {}  // For excalidraw to work
-  },
+  define: viteEnv,
   build: {
     sourcemap: true,
   }
