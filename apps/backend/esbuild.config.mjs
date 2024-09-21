d0cb1c7f237c8eeb4cce9b9b15eb0e4f52c1525b
Rahul Badenkal <rahulbadenkal@gmail.com>
2024-09-21 12:27:31 +0530

    updated backend build file

--- apps/backend/esbuild.config.mjs
+++ apps/backend/esbuild.config.mjs
@@ -1,6 +1,11 @@
 import { build } from 'esbuild';
 import { resolve } from 'path';
 import { copy } from 'esbuild-plugin-copy';
+import fs from "fs"
+
+const packageJsonFile = JSON.parse(fs.readFileSync(resolve(process.cwd(), "../", "../", "package.json"), "utf-8"))
+const dependencies = Object.keys(packageJsonFile.dependencies)
+const devDependencies = Object.keys(packageJsonFile.devDependencies)
 
 
 await build({
@@ -11,7 +16,7 @@ await build({
   format: 'esm',
   minify: false,
   sourcemap: true,
-  external: ["node_modules/*"],
+  external: dependencies.concat(devDependencies),
   plugins: [
     copy({
       // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
