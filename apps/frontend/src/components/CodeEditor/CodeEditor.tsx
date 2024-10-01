1daa6e2e3e2f7b182643105c1ed995e1be184e62
Rahul Badenkal <rahulbadenkal@gmail.com>
2024-10-02 04:49:10 +0530

    Added code mirror syntax highlighting and few other css changes

--- apps/frontend/src/components/CodeEditor/CodeEditor.tsx
+++ apps/frontend/src/components/CodeEditor/CodeEditor.tsx
@@ -1,8 +1,14 @@
 import { createEffect, createSignal, onMount } from "solid-js";
 
 import {EditorState, Extension} from "@codemirror/state"
-import {EditorView, keymap, lineNumbers, highlightActiveLineGutter  } from "@codemirror/view"
-import {defaultKeymap, indentMore} from "@codemirror/commands"
+import {EditorView, keymap, lineNumbers, highlightActiveLineGutter, highlightSpecialChars, drawSelection, dropCursor, rectangularSelection, crosshairCursor, highlightActiveLine  } from "@codemirror/view"
+import {defaultKeymap, history, historyKeymap, indentMore} from "@codemirror/commands"
+import {defaultHighlightStyle, syntaxHighlighting, indentUnit, indentOnInput, bracketMatching, foldGutter, foldKeymap} from "@codemirror/language"
+import {autocompletion, completionKeymap, closeBrackets, closeBracketsKeymap} from "@codemirror/autocomplete"
+import {searchKeymap, highlightSelectionMatches} from "@codemirror/search"
+import {lintKeymap} from "@codemirror/lint"
+import {javascript} from "@codemirror/lang-javascript"
+import {python} from "@codemirror/lang-python"
 
 import * as Y from 'yjs'
 import { yCollab } from 'y-codemirror.next'
@@ -18,12 +24,35 @@ export type CodeEditorProps = {
   awareness?: Awareness,
 }
 
+const languageSpecificExtensions = (language: Language) => {
+  let extensions = []
+  switch (language) {
+    case Language.JS_NODE_20:
+    case Language.JS_NODE_20_ES6: {
+      extensions = [
+        javascript(),
+        indentUnit.of(" ".repeat(2)),
+      ]
+      break
+    }
+    case Language.PYTHON_3_12: {
+      extensions = [
+        python(),
+        indentUnit.of(" ".repeat(4)),
+      ]
+      break
+    }
+    default: {
+      break
+    }
+  }
+  return extensions
+}
 
 // TODO: Add support when a new instance of y.Text is passed
 export const CodeEditor = (props: CodeEditorProps) => {
   const [editorRef, setEditorRef] = createSignal<HTMLElement>();
 
-  const undoManager = new Y.UndoManager(props.yCode)
   let state: EditorState
   let view: EditorView
 
@@ -34,7 +63,13 @@ export const CodeEditor = (props: CodeEditorProps) => {
     }
 
     const keyCommands = [
+      ...closeBracketsKeymap,
       ...defaultKeymap,
+      ...searchKeymap,
+      ...historyKeymap,
+      ...foldKeymap,
+      ...completionKeymap,
+      ...lintKeymap,
       {
         key: "Tab",
         preventDefault: true,
@@ -45,13 +80,33 @@ export const CodeEditor = (props: CodeEditorProps) => {
     const extensions: Extension = [
       lineNumbers(),
       highlightActiveLineGutter(),
+      highlightSpecialChars(),
+      history(),
+      foldGutter(),
+      drawSelection(),
+      dropCursor(),
+      EditorState.allowMultipleSelections.of(true),
+      indentOnInput(),
+      syntaxHighlighting(defaultHighlightStyle, {fallback: true}),
+      bracketMatching(),
+      closeBrackets(),
+      // autocompletion(),
+      rectangularSelection(),
+      crosshairCursor(),
+      highlightActiveLine(),
+      highlightSelectionMatches(),
       keymap.of(keyCommands),
-      yCollab(props.yCode, null, {undoManager: false})
+
+      // language specific extensions
+      ...languageSpecificExtensions(props.language),
+
+      // yjs extension
+      yCollab(props.yCode, null, {undoManager:  new Y.UndoManager(props.yCode)})
     ];
 
     state = EditorState.create({
       doc: props.yCode.toString(),
-      extensions: extensions,
+      extensions,
     });
 
     view = new EditorView({
@@ -63,6 +118,7 @@ export const CodeEditor = (props: CodeEditorProps) => {
   onMount(() => {});
 
   createEffect(() => {
+    // on any concerned prop change, rebuild the entire editor
     recreate()
   })
 
